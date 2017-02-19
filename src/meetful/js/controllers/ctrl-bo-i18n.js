a09d3579eaa5fb1725ecf43ed2babc28414db576
javoche <arancibiajav@gmail.com>
2017-02-19 11:27:25 +0000

    meetful-xxx-i18n-crud-improvement

--- src/meetful/js/controllers/ctrl-bo-i18n.js
+++ src/meetful/js/controllers/ctrl-bo-i18n.js
@@ -18,9 +18,46 @@ angular.module('ctrl-bo-i18n', []).controller('ctrl-bo-i18n', ['$scope', '$rootS
             field: 'code'
         },
         actions: {
-            save: false
+            save: true
+        },
+        validate: {
+            save: (data, next) => {
+                if (!data.code) return appGui.warningMessage('Code required');
+                if (!data.content) return appGui.warningMessage('Content description');
+                if (!data._category) return appGui.warningMessage('i18n category should be set first.');
+
+
+                return appApi.texts.exists(Object.assign({
+                    code: data.code
+                }, s.isEdit() ? {
+                    __rules: {
+                        _id: {
+                            $ne: data._id
+                        }
+                    }
+                } : {})).then(exists => {
+                    if (exists) return appGui.warningMessage('Code already exists for another registry.');
+                    next();
+                });
+
+
+                next();
+            }
         }
     });
+
+    appApi.categories.get({
+        code: 'i18n'
+    }).then(res => {
+        s.data._category = res._id;
+    });
+
+    s.encode = () => {
+        s.data.content = window.encodeURIComponent(s.data.content);
+    };
+    s.decode = () => {
+        s.data.content = window.decodeURIComponent(s.data.content);
+    };
 
     appApi.texts.custom('i18nConfig', {
         appName: appSettings.appName
