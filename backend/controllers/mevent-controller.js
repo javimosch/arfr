1535a1e05d596fbd40fd0678a98638d9c6c11f38
javoche <arancibiajav@gmail.com>
2017-02-19 11:16:51 +0000

    meetful-xxx-backend-event-save

--- backend/controllers/mevent-controller.js
+++ backend/controllers/mevent-controller.js
@@ -1,40 +1,22 @@
-var Mongorito = require('mongorito');
-//var MEvent = require('../model/mongorito-schemas').MEvent;
 var co = require('co');
-var MONGOOSE_URI = require('../model/backend-database').dbURI;
+const controllers = require('../model/backend-controllers-manager');
 
 module.exports = {
-   // test: test
-}
+    save: (data, cb) => {
 
-/*
-function test(data,cb) {
-    co(function*() {
-        "use strict"
-        
-        console.log('DEBUG: conectando ....');
-        yield Mongorito.connect(MONGOOSE_URI);
-        console.log('DEBUG: contectado.');
-
-        function* createPost() {
-            console.log('DEBUG: creando post');
-            var post = new MEvent({
-                title: 'Node.js with --harmony rocks!',
-                body: 'Long post body',
-                author: {
-                    name: 'John Doe'
-                },
-                comments: []
-            });
-            post.set('age', 22);
-            yield post.save();
-            console.log('DEBUG: creado post ', post);
+        //If there is not any status, lets go open
+        if (!data.status) {
+            data.status = 'open';
         }
 
+        //If owner is not in the _users array, let put it in
+        if (data._owner) {
+            data._users = data._users || [];
+            if (data._users.filter(u => (u && u._id || u) == (data._owner && data._owner._id || data._owner)).length == 0) {
+                data._users.push(data._owner);
+            }
+        }
 
-        yield createPost();
-        
-        yield Mongorito.disconnect();
-
-    }).catch(cb);
-}*/
+        return controllers.mevent.core.save(data, cb);
+    }
+}
