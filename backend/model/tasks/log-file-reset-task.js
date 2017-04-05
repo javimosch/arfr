4e0b6b86e0aec33bc813f4802abebe7ec4874f37
javoche <arancibiajav@gmail.com>
2017-04-05 15:14:08 +0000

    xxx-new-task-log-file-reset

--- backend/model/tasks/log-file-reset-task.js
+++ backend/model/tasks/log-file-reset-task.js
@@ -1,8 +1,10 @@
 var filesize = require('file-size');
 var sander = require('sander');
+var fs = require('fs');
 var path = require('path');
-var resolver = require(path.join(process.cwd(),'lib/resolver'));
-var name = 'log-file-reset' 
+var resolver = require(path.join(process.cwd(), 'lib/resolver'));
+var name = 'log-file-reset'
+var logger = resolver.logger().get('SERVER', 'TASK-' + name.toUpperCase());
 module.exports = {
     name: name,
     interval: 1000 * 60 * 10, //each minutes
@@ -10,7 +12,35 @@ module.exports = {
     startupInterval: true,
     //startupIntervalDelay: 1000,
 };
-function handler(){
-    var logger = resolver.logger().get('SERVER','TASK-'+name.toUpperCase());
-    logger.debug('Start');
+
+function handler() {
+    sander.fstat(sander.openSync(path.join(process.cwd(), 'log.txt'), 'r')).then(res => {
+        var sizeInMb = filesize(res.size).to('MB');
+        var _path = path.join(process.cwd(), 'log.txt');
+        var fileController = resolver.databaseControllers().file;
+        if (sizeInMb > 5) {
+            fileController.uploadFromFileSystem({
+                path: _path,
+                filename: 'log.txt'
+            }).then(file => {
+                resetLogFile(_path);
+                logger.debugSave('Log file written to database and reseted', file);
+            });
+        }
+        else {
+            logger.debug('Log file is < 5mb, reset ignore.')
+        }
+    }).catch(err => {
+        logger.error('Stat', err);
+    });
+}
+
+function resetLogFile(filePath) {
+    sander.unlink(filePath).then(() => {
+        sander.writeFileSync(filePath, '', {
+            encoding: 'utf-8'
+        });
+    }).catch(err => {
+        logger.error(err);
+    });
 }
