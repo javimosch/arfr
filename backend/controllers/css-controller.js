c9fab3db106ca4d7d491972b65759ab02ffabac8
javoche <arancibiajav@gmail.com>
2017-04-05 15:12:51 +0000

    xxx-css-fast-save-improved

--- backend/controllers/css-controller.js
+++ backend/controllers/css-controller.js
@@ -1,14 +1,9 @@
+var path = require('path');
+var resolver = require(path.join(process.cwd(), 'lib/resolver'));
 var controllers = require('../model/backend-controllers-manager');
 var atob = require('atob');
-var Logger = controllers.logs.createLogger({
-    name: "API",
-    category: "CSS"
-});
-
-//var coreUtils = require(process.cwd()+'/lib/core/utils');
-
+var Logger = resolver.logger().get('API','CSS');
 var fs = require('fs');
-var path = require('path');
 var sander = require('sander');
 var cssbeautify = require('cssbeautify');
 
@@ -17,24 +12,33 @@ module.exports = {
 };
 
 function saveLocalFile(data, cb) {
+    
+    data.appName = data.appName || resolver.env().APP_NAME;
+    
+    Logger.debug('Using appName ',data.appName);
 
     if (fs.existsSync(path.join(process.cwd(), 'src', data.appName, 'css', data.fileName))) {
         var result = atob(data.encoded);
         result = cssbeautify(result);
+
+        Logger.debug('SAVING'/*, {
+            fileName: data.fileName,
+            appName: data.appName,
+            where: path.join(process.cwd(), 'src', data.appName, 'css')
+        }*/);
+
         sander.writeFile(path.join(process.cwd(), 'src', data.appName, 'css'), data.fileName, result).then(res => {
-
-            //Logger.debug('SAVE RESULT ', res);
-
-            Logger.debug('SAVING CSS', {
+            Logger.debug('SAVED'/*, {
                 fileName: data.fileName,
                 appName: data.appName,
                 exists: fs.existsSync(path.join(process.cwd(), 'src', data.appName, 'css', data.fileName)),
                 content: atob(data.encoded)
-            });
-
+            }*/);
             return cb(null, 'Saved!');
-
-
+        });
+    }else{
+        Logger.warn('The file do not exists', {
+            path: path.join(process.cwd(), 'src', data.appName, 'css', data.fileName)
         });
     }
 
